764ddc207240a1ea922e36a2499b349e23e0c73b
lukia zhao <yifengzhao0530@gmail.com>
2018-08-08 22:34:39 +1000

    code cleaned up

--- GeoTracking/app/src/main/java/com/rmit/geotracking/MainActivity.java
+++ GeoTracking/app/src/main/java/com/rmit/geotracking/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.rmit.geotracking.service.TestTrackingService;
@@ -20,36 +21,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab_view);
+        setContentView(R.layout.activity_main);
+
+        View button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goTrackable(v);
+            }
+        });
 
 
         TestTrackingService.test(this);
 
         /* spinner **/
-//        String[] data = new String[]{"Category first", "selection 2", "selection 3", "selection 4", "Category last"};
-        ArrayList<String> data = new ArrayList<>();
-        data.add("selection 1"); data.add("selection 3"); data.add("category last");
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter adapterSpin = new ArrayAdapter(this, android.R.layout.simple_spinner_item, data);
-        adapterSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(adapterSpin);
-//        spinner.setSelection(adapterSpin.getCount());
-//        spinner.setSelection(adapterSpin.getCount());
-
-        /* Recycler view of trackables **/
-
-        RecyclerView recyclerView = findViewById(R.id.recycler_view_trackables);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        // create an adapter object
-        RecyclerAdapter adapter = new RecyclerAdapter(this);
-
-        // associate the recycler view with the adapter
-        recyclerView.setAdapter(adapter);
+////        String[] data = new String[]{"Category first", "selection 2", "selection 3", "selection 4", "Category last"};
+//        ArrayList<String> data = new ArrayList<>();
+//        data.add("selection 1"); data.add("selection 3"); data.add("category last");
+//        Spinner spinner = findViewById(R.id.spinner);
+//        ArrayAdapter adapterSpin = new ArrayAdapter(this, android.R.layout.simple_spinner_item, data);
+//        adapterSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//        spinner.setAdapter(adapterSpin);
+////        spinner.setSelection(adapterSpin.getCount());
+//
+//
+//        /* Recycler view of trackables **/
+//
+//        RecyclerView recyclerView = findViewById(R.id.recycler_view_trackables);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        // create an adapter object
+//        RecyclerAdapter adapter = new RecyclerAdapter(this);
+//
+//        // associate the recycler view with the adapter
+//        recyclerView.setAdapter(adapter);
     }
 
 
+    public void goTrackable (View v){
+        Intent myIntent = new Intent(this, TrackableActivity.class);
+        startActivity(myIntent);
+    }
 
 
 }
--- GeoTracking/app/src/main/java/com/rmit/geotracking/TrackableActivity.java
+++ GeoTracking/app/src/main/java/com/rmit/geotracking/TrackableActivity.java
@@ -4,13 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class TrackableActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab_view);
+        setContentView(R.layout.activity_trackable);
+
+
+        /* spinner **/
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("selection 1"); data.add("selection 3"); data.add("category last");
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter adapterSpin = new ArrayAdapter(this, android.R.layout.simple_spinner_item, data);
+        adapterSpin.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapterSpin);
+
+
+        spinner.setSelection(data.size() - 1);
+
 
         /* Recycler view of trackables **/
 
