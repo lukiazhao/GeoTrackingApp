b73e2cbe985ca834900552b32ed6f80cd3e62b93
rmit-s3512993-Zhongnian-Lu <luzhongnian1991@gmail.com>
2018-08-28 17:39:56 +1000

    [TrackingListAdapter] move the array generating function from adapter to model

--- GeoTracking/app/src/main/java/com/rmit/geotracking/adapter/TrackingListAdapter.java
+++ GeoTracking/app/src/main/java/com/rmit/geotracking/adapter/TrackingListAdapter.java
@@ -2,6 +2,7 @@ package com.rmit.geotracking.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.rmit.geotracking.R;
 import com.rmit.geotracking.controller.CheckTrackingListener;
+import com.rmit.geotracking.controller.EditTrackingListener;
 import com.rmit.geotracking.model.TrackManager;
 import com.rmit.geotracking.model.Tracking;
 
@@ -19,39 +21,38 @@ import java.util.Set;
 
 public class TrackingListAdapter extends BaseAdapter {
     private Map<String, Tracking> trackingMap;
-    LayoutInflater inflater;
-    Context context;
+    private Context context;
     private String [] keyArray;
+    private TrackManager manager;
 
-    public TrackingListAdapter(Context context){
-        System.out.println("Create TrackingList adapter");
+    public TrackingListAdapter(Context context, TrackManager manager){
+    //    System.out.println("Create TrackingList adapter");
         this.context = context;
         this.trackingMap = TrackManager.getSingletonInstance(context).getTrackingMap();
-        this.keyArray = generateKeyArray(trackingMap.keySet());
+        this.manager = manager;
+        this.keyArray = manager.generateTrackingKeyArray();
+    //    this.keyArray = generateKeyArray(trackingMap.keySet());
         System.out.println("generateKeyArray: keyset:  " + trackingMap.keySet());
-
-        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
   //      System.out.println(trackingMap.get(0).getTitle());
     }
 
     //gerenate an array of keys to help get value in getItem() method
-    public String [] generateKeyArray(Set<String> keyset){
-        String [] outputarray = new String [keyset.size()];
-        int position = 0;
-
-
-        for (String key : keyset) {
-            outputarray[position] = key;
-            position++;
-            System.out.println("Checkarray!" + key);
-        }
-
-        return outputarray;
-    }
+//    public String [] generateKeyArray(Set<String> keyset){
+//        String [] outputarray = new String [keyset.size()];
+//        int position = 0;
+//
+//        for (String key : keyset) {
+//            outputarray[position] = key;
+//            position++;
+//            System.out.println("Checkarray!" + key);
+//        }
+//
+//        return outputarray;
+//    }
 
     @Override
     public int getCount() {
-        return trackingMap.size();
+        return manager.generateTrackingKeyArray().length;
     }
 
     @Override
@@ -68,20 +69,26 @@ public class TrackingListAdapter extends BaseAdapter {
     public View getView(int position, View view, ViewGroup viewGroup) {
 
         View v = LayoutInflater.from(context).inflate(R.layout.single_tracking_view, viewGroup, false);
+        System.out.println(getCount());
 
         TextView trackingTitleView = (TextView) v.findViewById(R.id.trackingTitleTextView);
-        TextView trackingMeetView = (TextView) v.findViewById(R.id.trackingMeetTextView);
-        TextView trackingLocationView = (TextView) v.findViewById(R.id.trackingLocationTextView);
+//        TextView trackingMeetView = (TextView) v.findViewById(R.id.trackingMeetTextView);
+//        TextView trackingLocationView = (TextView) v.findViewById(R.id.trackingLocationTextView);
 
         trackingTitleView.setText(trackingMap.get(keyArray[position]).getTitle());
-        trackingMeetView.setText(trackingMap.get(keyArray[position]).getMeetTime().toString());
-        trackingLocationView.setText(trackingMap.get(keyArray[position]).getMeetLocation());
+//        trackingMeetView.setText(trackingMap.get(keyArray[position]).getMeetTime().toString());
+//        trackingLocationView.setText(trackingMap.get(keyArray[position]).getMeetLocation());
 
         Button trackingViewButton = (Button) v.findViewById(R.id.trackingViewButton);
         Button trackingEditButton = (Button) v.findViewById(R.id.trackingEditButton);
 
         trackingViewButton.setOnClickListener(new CheckTrackingListener(context, trackingMap.get(keyArray[position]), this));
-
+        trackingEditButton.setOnClickListener(new EditTrackingListener(context, trackingMap.get(keyArray[position]), this));
         return v;
+    }
+
+    public void updateKeyArray(String [] keyArray){
+        this.keyArray = keyArray;
+
     }
 }
