eb29e8e30ce5137a4913a158921a60555ea4c049
rmit-s3512993-Zhongnian-Lu <luzhongnian1991@gmail.com>
2018-08-29 22:40:36 +1000

    [Trackable] route: complete route display

--- GeoTracking/app/src/main/java/com/rmit/geotracking/controller/ViewRouteListener.java
+++ GeoTracking/app/src/main/java/com/rmit/geotracking/controller/ViewRouteListener.java
@@ -12,6 +12,7 @@ import com.rmit.geotracking.R;
 import com.rmit.geotracking.adapter.RouteListAdapter;
 import com.rmit.geotracking.model.Trackable;
 import com.rmit.geotracking.service.TrackingService;
+import com.rmit.geotracking.view.TrackableActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +22,14 @@ public class ViewRouteListener implements View.OnClickListener {
     private Trackable trackable;
     private List<TrackingService.TrackingInfo> trackingInfoList;
     private Context context;
+    private boolean hasInfo;
+
 
     public ViewRouteListener(Context context, Trackable trackable){
         this.trackable = trackable;
         this.context = context;
         trackingInfoList = TrackingService.getSingletonInstance(context).getTrackingInfoList();
+        hasInfo = false;
     }
 
     @Override
@@ -40,8 +44,14 @@ public class ViewRouteListener implements View.OnClickListener {
         Button confirmbutton = (Button) v.findViewById(R.id.route_confirm);
 
         title2.setText(trackable.getName());
-        routelv.setAdapter(new RouteListAdapter(context, createRouteList()));
+        List<String> routeList = createRouteList();
 
+        if(hasInfo) {
+            routelv.setAdapter(new RouteListAdapter(context, routeList));
+        } else {
+            ((TrackableActivity) context).showNoRouteToast();
+            return;
+        }
         builder.setView(v);
 
         AlertDialog dialog = builder.create();
@@ -49,12 +59,13 @@ public class ViewRouteListener implements View.OnClickListener {
         confirmbutton.setOnClickListener(new DialogDismissListener(dialog));
     }
 
-    public List<String> createRouteList(){
+    public List<String> createRouteList() {
         List<String> routelist = new ArrayList<String>();
 
         for(TrackingService.TrackingInfo trackingInfo : trackingInfoList) {
             if(trackingInfo.trackableId == trackable.getId()) {
                 routelist.add(trackingInfo.latitude + "  " + trackingInfo.longitude);
+                hasInfo = true;
             }
         }
 
